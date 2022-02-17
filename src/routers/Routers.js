6faaf0264d74874016af6162fd14a6c0f1fa14c8
ehpip <afifpondokprogrammer@gmail.com>
2022-02-17 13:14:59 +0700

    Update 17-2-2022

--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 
 import HomeScreen from '../screens/HomeScreen'
 import DashboardPengajuanScreen from '../screens/DashboardPengajuanScreen'
+import IsiSaldoScreen from '../screens/IsiSaldoScreen'
 
 export default class Routers extends Component {
   render() {
     return (
         // <HomeScreen />
-        <DashboardPengajuanScreen />
+        // <DashboardPengajuanScreen />
+        <IsiSaldoScreen />
     )
   }
 }
