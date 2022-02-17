86e7e4cd84464689a3ff809a96a4026be2bc8680
ehpip <afifpondokprogrammer@gmail.com>
2022-02-17 12:06:33 +0700

    Dashboard Pengajuan Screen

--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 
 import HomeScreen from '../screens/HomeScreen'
+import DashboardPengajuanScreen from '../screens/DashboardPengajuanScreen'
 
 export default class Routers extends Component {
   render() {
     return (
-        <HomeScreen />
+        // <HomeScreen />
+        <DashboardPengajuanScreen />
     )
   }
 }
