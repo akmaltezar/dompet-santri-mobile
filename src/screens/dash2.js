46191374386fbbbd0b75dd6a89470b7a4a0ea89d
ehpip <afifpondokprogrammer@gmail.com>
2022-03-18 11:28:11 +0700

    integrate API GET dashboard riwayat pengajuan

--- src/screens/dash2.js
+++ src/screens/dash2.js
@@ -1,14 +1,72 @@
 import React from 'react';
-import {View, Text, Dimensions, TouchableOpacity} from 'react-native';
+import {
+  View,
+  Text,
+  Dimensions,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const ww = Dimensions.get('window').width;
 
 export default class Dashboard2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      token: '',
+    };
+  }
+
+  componentDidMount() {
+    AsyncStorage.getItem('token')
+      .then(value => {
+        if (value != null) {
+          this.setState({token: value});
+        } else {
+          this.props.navigation.replace('LoginScreen');
+        }
+      })
+      .then(() => {
+        this.getRiwayatPengajuan();
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    this.unsubscribe = this.props.navigation.addListener('focus', () => {
+      this.getRiwayatPengajuan();
+    });
+  }
+
+  getRiwayatPengajuan() {
+    var requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+      headers: {
+        Authorization: `Bearer ${this.state.token}`,
+      },
+    };
+
+    fetch(
+      'https://aplikasi-santri.herokuapp.com/api/historydashboard',
+      requestOptions,
+    )
+      .then(response => response.json())
+      .then(result => {
+        console.log(result);
+        this.setState({data: result.data});
+      })
+      .catch(error => console.log('error', error));
+  }
+
   render() {
     const navigation = this.props.navigation;
     return (
-      <View style={{alignItems: 'center', flex: 1, backgroundColor: 'white'}}>
-        {/* <View style={{backgroundColor:'#8388FF',width:ww,height:50,justifyContent:'center',alignItems:'center',}}>
+      <View style={{flex: 1, backgroundColor: 'white'}}>
+        <ScrollView>
+          <View style={{alignItems: 'center', marginTop: 15}}>
+            {/* <View style={{backgroundColor:'#8388FF',width:ww,height:50,justifyContent:'center',alignItems:'center',}}>
                 <Text style={{color:'white',fontSize:16,marginLeft:10}}>Dashboard</Text>
             </View>
             <View style={{flexDirection:'row',justifyContent:'space-evenly',alignItems:'center',width:ww,height:50,marginBottom:50}}>
@@ -18,7 +76,7 @@ export default class Dashboard2 extends React.Component {
                     <View style={{backgroundColor:'#8388FF',height:3,width:100,top:10}} />
                 </View>
             </View> */}
-        <View
+            {/* <View
           style={{
             justifyContent: 'center',
             alignItems: 'center',
@@ -194,7 +252,122 @@ export default class Dashboard2 extends React.Component {
               Lihat data
             </Text>
           </TouchableOpacity>
-        </View>
+        </View> */}
+            {this.state.data.map((value, index) => {
+              return (
+                <View
+                  key={index}
+                  style={{
+                    width: '90%',
+                    backgroundColor: '#FFF',
+                    elevation: 3,
+                    height: 100,
+                    borderRadius: 5,
+                    paddingHorizontal: 30,
+                    marginBottom: 15,
+                    justifyContent: 'center',
+                  }}>
+                  <View
+                    style={{
+                      flexDirection: 'row',
+                      width: '100%',
+                      justifyContent: 'space-between',
+                    }}>
+                    <View
+                      style={{
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        flexDirection: 'row',
+                      }}>
+                      <View
+                        style={{
+                          height: 10,
+                          width: 10,
+                          backgroundColor: '#FFA216',
+                          borderRadius: 5,
+                          marginRight: 10,
+                          elevation: 3,
+                        }}
+                      />
+                      <View>
+                        <Text style={{fontFamily: 'Montserrat-SemiBold'}}>
+                          Isi Saldo
+                        </Text>
+                        <Text style={{fontFamily: 'Montserrat-Regular'}}>
+                          {value.created_at.substr(0, 10)}
+                        </Text>
+                      </View>
+                    </View>
+                    {value.status === 'Success' ? (
+                      <View
+                        style={{
+                          width: 100,
+                          height: 25,
+                          backgroundColor: '#12E100',
+                          justifyContent: 'center',
+                          alignItems: 'center',
+                          borderRadius: 5,
+                          elevation: 3,
+                        }}>
+                        <Text
+                          style={{
+                            fontSize: 12,
+                            fontFamily: 'Montserrat-SemiBold',
+                            color: '#FFF',
+                          }}>
+                          {value.status}
+                        </Text>
+                      </View>
+                    ) : (
+                      <View
+                        style={{
+                          width: 100,
+                          height: 25,
+                          backgroundColor: '#E31212',
+                          justifyContent: 'center',
+                          alignItems: 'center',
+                          borderRadius: 5,
+                          elevation: 3,
+                        }}>
+                        <Text
+                          style={{
+                            fontSize: 12,
+                            fontFamily: 'Montserrat-SemiBold',
+                            color: '#FFF',
+                          }}>
+                          {value.status}
+                        </Text>
+                      </View>
+                    )}
+                  </View>
+                  <TouchableOpacity
+                    style={{
+                      backgroundColor: '#8388FF',
+                      width: '100%',
+                      height: 25,
+                      borderRadius: 5,
+                      elevation: 3,
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      marginTop: 12,
+                    }}>
+                    <Text
+                      onPress={() =>
+                        this.props.navigation.navigate('DetailRiwayat')
+                      }
+                      style={{
+                        fontSize: 12,
+                        fontFamily: 'Montserrat-SemiBold',
+                        color: '#FFF',
+                      }}>
+                      Lihat Data
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              );
+            })}
+          </View>
+        </ScrollView>
       </View>
     );
   }
