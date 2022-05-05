f40f23a14de5f61930fbbcb72799895c417cafb1
akmaltezar <akmalremake007@gmail.com>
2022-05-05 21:02:33 +0700

    fix styling on register screen

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -2,18 +2,20 @@ import React, {Component} from 'react';
 import {
   Text,
   StyleSheet,
+  Alert,
   View,
   TouchableOpacity,
   TextInput,
   Dimensions,
   ActivityIndicator,
-  Image,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
 
-const ww = Dimensions.get('window').width;
-
-export default class App extends Component {
+export default class RegisterScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -21,47 +23,23 @@ export default class App extends Component {
       email: '',
       password: '',
       password_confirmation: '',
-      roles: 'merchant',
+      roles: 'customer',
       loading: false,
+      eye: true,
+      eye2: true,
     };
   }
 
   Register = () => {
     this.setState({loading: true});
-
-    // fetch('https://aplikasi-santri.herokuapp.com/api/registeruser', {
-    //   method: 'POST',
-    //   headers: {
-    //     Accept: 'application/json',
-    //     'Content-Type': 'application/json',
-    //   },
-    //   body: JSON.stringify({
-    //     name: this.state.name,
-    //     email: this.state.email,
-    //     password: this.state.password,
-    //     password_confirmation: this.state.password_confirmation,
-    //     roles: this.state.roles,
-    //   }),
-    // })
-    //   .then(response => response.json())
-    //   .then(result => {
-    //     console.log(result);
-    //     if (result.code === ) {
-
-    //     }
-    //     this.props.navigation.replace('LoginScreen');
-    //   })
-    //   .catch(error => {
-    //     console.log('error', error);
-    //   })
-    var formdata = new FormData();
+    let formdata = new FormData();
     formdata.append('name', this.state.name);
     formdata.append('email', this.state.email);
     formdata.append('password', this.state.password);
     formdata.append('password_confirmation', this.state.password_confirmation);
     formdata.append('roles', this.state.roles);
 
-    var requestOptions = {
+    const requestOptions = {
       method: 'POST',
       body: formdata,
       redirect: 'follow',
@@ -78,7 +56,7 @@ export default class App extends Component {
           alert('Register Success');
           this.props.navigation.navigate('LoginScreen');
         } else {
-          alert('Fill the form correctly.');
+          alert(result.password);
         }
       })
       .catch(error => console.log('error', error))
@@ -94,198 +72,77 @@ export default class App extends Component {
 
   render() {
     return (
-      // <View style={{flex: 1, justifyContent: 'center', backgroundColor: '#FFF'}}>
-      //   <Text style={[styles.text1, styles.ds]}>Dompet Santri</Text>
-      //   <Text style={[styles.text1, styles.rg]}>Register</Text>
-
-      //   <View style={styles.tibox}>
-      //     <View style={styles.imgisi}>
-      //       <Image
-      //         style={styles.imgg}
-      //         source={require('../assets/images/org.png')}
-      //       />
-      //       <TextInput
-      //         placeholder="Nama Lengkap"
-      //         style={styles.ti}
-      //         onChangeText={value => this.setState({name: value})}
-      //       />
-      //     </View>
-      //     <View style={styles.imgisi}>
-      //       <Image
-      //         style={styles.imgg}
-      //         source={require('../assets/images/mail.png')}
-      //       />
-      //       <TextInput
-      //         placeholder="Email"
-      //         style={styles.ti}
-      //         onChangeText={value => this.setState({email: value})}
-      //       />
-      //     </View>
-      //     <View style={styles.imgisi}>
-      //       <Image
-      //         style={styles.imgg}
-      //         source={require('../assets/images/lock.png')}
-      //       />
-      //       <TextInput
-      //         placeholder="Kata Sandi"
-      //         style={styles.ti}
-      //         onChangeText={value => this.setState({password: value})}
-      //       />
-      //     </View>
-      //   </View>
-
-      //   <View style={styles.tibutton}>
-      //     <TouchableOpacity style={styles.df} onPress={() => this.Register()}>
-      //       <Text style={styles.dftext}>DAFTAR</Text>
-      //     </TouchableOpacity>
-
-      //     <TouchableOpacity
-      //       style={[styles.df, styles.bu2]}
-      //       onPress={() => this.props.navigation.navigate('LoginScreen')}>
-      //       <Text style={styles.dftext}>MASUK</Text>
-      //     </TouchableOpacity>
-      //   </View>
-      // </View>
-      <View
-        style={{
-          justifyContent: 'center',
-          alignItems: 'center',
-          flex: 1,
-          backgroundColor: 'white',
-        }}>
-        <View style={{alignSelf: 'flex-start', marginLeft: 50}}>
-          <Text
-            style={{
-              color: '#8388FF',
-              fontSize: 24,
-              fontFamily: 'Montserrat-Bold',
-            }}>
-            Dompet Santri
-          </Text>
-          <Text
-            style={{
-              color: '#8388FF',
-              fontSize: 24,
-              alignSelf: 'flex-start',
-              fontFamily: 'Montserrat-Bold',
-            }}>
-            Register
-          </Text>
+      <View style={styles.container}>
+        <View style={styles.titleView}>
+          <Text style={styles.title}>Dompet Santri</Text>
+          <Text style={styles.title}>Register</Text>
         </View>
-        <View style={{marginTop: 20, marginBottom: 30}}>
-          <View>
-            <View style={{flexDirection: 'row', alignItems: 'center'}}>
-              <Icon
-                name="account-outline"
-                size={25}
-                color="#8388FF"
-                // style={{width: 24, height: 24, marginRight: 10}}
-              />
-              <TextInput
-                placeholder="Nama Lengkap"
-                style={{width: 220}}
-                onChangeText={name => this.setState({name})}
-                keyboardType="default"
-              />
-            </View>
-            <View
-              style={{backgroundColor: '#8388FF', height: 1, width: ww - 100}}
+        <View style={{alignItems: 'center'}}>
+          <View style={styles.inputView}>
+            <Icon name="account-outline" size={28} color="#8388FF" />
+            <TextInput
+              placeholder="Nama Lengkap"
+              onChangeText={name => this.setState({name})}
+              style={styles.input}
             />
           </View>
-          <View>
-            <View style={{flexDirection: 'row', alignItems: 'center'}}>
-              <Icon
-                name="email-outline"
-                size={25}
-                color="#8388FF"
-                // style={{width: 24, height: 24, marginRight: 10}}
-              />
-              <TextInput
-                placeholder="Email"
-                style={{width: 220}}
-                onChangeText={email => this.setState({email})}
-                keyboardType="email-address"
-                autoCapitalize="none"
-              />
-            </View>
-            <View
-              style={{backgroundColor: '#8388FF', height: 1, width: ww - 100}}
+          <View style={styles.inputView}>
+            <Icon name="email-outline" size={28} color="#8388FF" />
+            <TextInput
+              placeholder="Alamat Email"
+              onChangeText={email => this.setState({email})}
+              autoCapitalize="none"
+              style={styles.input}
             />
           </View>
-          <View>
-            <View style={{flexDirection: 'row', alignItems: 'center'}}>
-              <Icon
-                name="lock-outline"
-                size={25}
-                color="#8388FF"
-                // style={{width: 24, height: 24, marginRight: 10}}
-              />
-              <TextInput
-                placeholder="Kata Sandi"
-                style={{width: 220}}
-                onChangeText={password => this.setState({password})}
-                secureTextEntry={true}
-              />
-            </View>
-            <View
-              style={{backgroundColor: '#8388FF', height: 1, width: ww - 100}}
+
+          <View style={styles.inputView2}>
+            <Icon name="lock-outline" size={28} color="#8388FF" />
+            <TextInput
+              onChangeText={password => this.setState({password})}
+              placeholder="Kata Sandi"
+              secureTextEntry={this.state.eye}
+              style={styles.input}
             />
+            <TouchableOpacity
+              onPressOut={() => this.setState({eye: !this.state.eye})}>
+              <Icon name="eye-outline" size={28} color="#8388FF" />
+            </TouchableOpacity>
           </View>
-          <View>
-            <View style={{flexDirection: 'row', alignItems: 'center'}}>
-              <Icon
-                name="lock-outline"
-                size={25}
-                color="#8388FF"
-                // style={{width: 24, height: 24, marginRight: 10}}
-              />
-              <TextInput
-                placeholder="Konfirmasi Kata Sandi"
-                style={{width: 220}}
-                onChangeText={password_confirmation =>
-                  this.setState({password_confirmation})
-                }
-                secureTextEntry={true}
-              />
-            </View>
-            <View
-              style={{backgroundColor: '#8388FF', height: 1, width: ww - 100}}
+          <View style={styles.inputView2}>
+            <Icon name="lock-check-outline" size={28} color="#8388FF" />
+            <TextInput
+              onChangeText={password_confirmation =>
+                this.setState({password_confirmation})
+              }
+              secureTextEntry={this.state.eye2}
+              placeholder="Konfirmasi Kata Sandi"
+              style={styles.input}
             />
+            <TouchableOpacity
+              onPressOut={() => this.setState({eye2: !this.state.eye2})}>
+              <Icon name="eye-outline" size={28} color="#8388FF" />
+            </TouchableOpacity>
           </View>
         </View>
-        <View>
+        <View
+          style={{
+            alignItems: 'center',
+            marginTop: '10%',
+          }}>
           <TouchableOpacity
-            onPress={() => this.Register()}
-            style={{
-              backgroundColor: '#8388FF',
-              height: 45,
-              width: ww - 100,
-              alignItems: 'center',
-              justifyContent: 'center',
-              borderRadius: 10,
-              marginBottom: 20,
-            }}>
+            style={styles.button}
+            onPress={() => this.Register()}>
             {this.state.loading ? (
-              <ActivityIndicator size={25} color="#FFF" />
+              <ActivityIndicator size={24} color="#FFFFFF" />
             ) : (
-              <Text style={{color: 'white', fontFamily: 'Montserrat-SemiBold'}}>
-                DAFTAR
-              </Text>
+              <Text style={styles.buttonText}>DAFTAR</Text>
             )}
           </TouchableOpacity>
           <TouchableOpacity
-            style={{
-              backgroundColor: '#8388FF',
-              height: 45,
-              width: ww - 100,
-              alignItems: 'center',
-              justifyContent: 'center',
-              borderRadius: 10,
-            }}
+            style={styles.button}
             onPress={() => this.props.navigation.navigate('LoginScreen')}>
-            <Text style={{color: 'white', fontFamily: 'Montserrat-SemiBold'}}>
-              MASUK
-            </Text>
+            <Text style={styles.buttonText}>MASUK</Text>
           </TouchableOpacity>
         </View>
       </View>
@@ -294,51 +151,57 @@ export default class App extends Component {
 }
 
 const styles = StyleSheet.create({
-  text1: {
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  titleView: {
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    paddingLeft: '10%',
+    marginBottom: '10%',
+  },
+  title: {
+    fontFamily: 'Montserrat-Bold',
+    fontSize: 30,
     color: '#8388FF',
-    fontWeight: 'bold',
-    fontSize: 24,
-    paddingLeft: 45,
   },
-  ds: {
-    marginTop: 65,
-  },
-  ti: {
-    width: 271,
+  inputView: {
+    flexDirection: 'row',
+    width: wp('80%'),
+    justifyContent: 'flex-start',
+    alignItems: 'center',
     borderBottomWidth: 1,
-    borderBottomColor: '#8388FF',
-    paddingLeft: 35,
+    borderColor: '#8388FF',
+    marginBottom: '2%',
   },
-  tibox: {
+  inputView2: {
+    flexDirection: 'row',
+    width: wp('80%'),
+    justifyContent: 'space-around',
     alignItems: 'center',
-    marginLeft: -15,
+    borderBottomWidth: 1,
+    borderColor: '#8388FF',
+    marginBottom: '2%',
   },
-  tibutton: {
-    alignItems: 'center',
-    marginTop: 50,
+  input: {
+    width: wp('65%'),
+    backgroundColor: '#FFFFFF',
   },
-  df: {
-    height: 45,
-    width: 271,
+  button: {
     backgroundColor: '#8388FF',
+    width: wp('80%'),
+    height: hp('6%'),
+    marginBottom: '3%',
+    borderRadius: 12,
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 5,
-    borderRadius: 10,
   },
-  dftext: {
-    color: 'white',
-    fontWeight: 'bold',
-  },
-  bu2: {
-    marginTop: 30,
-  },
-  imgisi: {
-    flexDirection: 'row',
-    marginTop: 10,
-  },
-  imgg: {
-    marginTop: 10,
-    left: 20,
+  buttonText: {
+    fontFamily: 'Montserrat-SemiBold',
+    color: '#FFFFFF',
+    fontSize: 17,
   },
 });
